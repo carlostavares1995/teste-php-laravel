9c11b3bea38336d789ba87192f08ea4887940450
Carlos Tavares <carlosrtf2014@gmail.com>
2024-08-03 21:56:43 -0300

    main: Creates the request validator for the document controller

---
+++ app/Http/Requests/DocumentRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DocumentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'json_file' => 'required|file|mimetypes:application/json,text/plain',
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        // Podemos fazer uma validação avançada no json aqui mas mantive uma validação simples nas rules
+    }
+}
