d6ecaea826e1ac9460f91e41ada871e83fc4603c
Carlos Tavares <carlosrtf2014@gmail.com>
2024-08-03 21:41:31 -0300

    main: Create a job to process documents

---
+++ app/Jobs/ProcessDocument.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Exceptions\ContentLengthExceededException;
+use App\Exceptions\InvalidTitleException;
+use App\Exceptions\MissingRequiredFieldsException;
+use App\Models\Category;
+use App\Models\Document;
+use App\Services\DocumentValidator;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class ProcessDocument implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        protected array $document,
+        protected DocumentValidator $validator
+    ) {
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $requiredFields = ['categoria', 'titulo', 'conteúdo'];
+        $missingFields = array_diff($requiredFields, array_keys($this->document));
+        if (!empty($missingFields)) {
+            throw new MissingRequiredFieldsException();
+        }
+
+        if (!$this->validator->validateContentLength($this->document['conteúdo'])) {
+            throw new ContentLengthExceededException();
+        }
+
+        if (!$this->validator->validateTitleByCategory($this->document['categoria'], $this->document['titulo'])) {
+            throw new InvalidTitleException();
+        }
+
+        $category = Category::getByName($this->document['categoria']);
+
+        Document::create([
+            'category_id' => $category->id,
+            'title' => $this->document['titulo'],
+            'contents' => $this->document['conteúdo']
+        ]);
+
+        Log::info('Documento processado com sucesso!', ['document' => $this->document]);
+    }
+}
